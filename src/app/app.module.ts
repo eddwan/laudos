88224205caae81f5f6f42a5d0bbcafb99999754c
Eddwan Hallen <eddwan@gmail.com>
2019-07-16 19:23:41 -0300

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule, RouteReuseStrategy } from '@angular/router'
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular'
+import { SplashScreen } from '@ionic-native/splash-screen/ngx'
+import { StatusBar } from '@ionic-native/status-bar/ngx'
+
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+
+import { NgxElectronModule } from 'ngx-electron'
+
+import { MyMaterialModule } from './material.module'
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, NgxElectronModule, MyMaterialModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import * as fs from 'fs';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+  
+  constructor(){
+    
+    fs.readdir('/Users/usuario/Desktop/laudos/laudos', (err, files) => {
+      files.forEach(file => {
+        console.log(file);
+        fs.readFile('/Users/usuario/Desktop/laudos/laudos/'+file, "utf8",  (err, data)  => {
+          if (err) throw err;
+          const obj = JSON.parse(data);
+          console.log(obj["nome"])
+        });
+      });
+    });
+    
+  }
+  
+}
